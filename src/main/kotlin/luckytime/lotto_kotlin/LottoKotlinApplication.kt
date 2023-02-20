72c06f5274a5e93410828662a48d14d693316bc7
b116104@naver.com <b116104@naver.com>
2023-02-20 23:10:10 +0900

    Initial commit

---
+++ src/main/kotlin/luckytime/lotto_kotlin/LottoKotlinApplication.kt
@@ -0,0 +1,11 @@
+package luckytime.lotto_kotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LottoKotlinApplication
+
+fun main(args: Array<String>) {
+    runApplication<LottoKotlinApplication>(*args)
+}
---
+++ src/test/kotlin/luckytime/lotto_kotlin/LottoKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package luckytime.lotto_kotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LottoKotlinApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
