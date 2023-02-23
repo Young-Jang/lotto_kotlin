6751dcd6bc99f80b2e91636cce4c746921ffaf9e
Young-Jang <b116104@naver.com>
2023-02-23 10:37:51 +0900

    Update HtmlController.kt
    add controller (translate from java)

--- src/main/kotlin/luckytime/lotto_kotlin/rest/HtmlController.kt
+++ src/main/kotlin/luckytime/lotto_kotlin/rest/HtmlController.kt
@@ -1,13 +1,38 @@
 package luckytime.lotto_kotlin.rest
 
-import org.springframework.stereotype.Controller
-import org.springframework.ui.Model
+import luckytime.config.ControllerDocumentation
+import luckytime.lotto.application.LottoService
+import luckytime.lotto.rest.DTO.GetLuckyNumRspDTO
+import org.json.simple.parser.ParseException
+import org.slf4j.LoggerFactory
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
 
-@Controller
-class HtmlController {
-    @RequestMapping("/")
-    fun index(model: Model) : String{
-        return "index"
+@RestController
+@RequestMapping("/lotto/")
+@RequiredArgsConstructor
+@ControllerDocumentation
+class LottoController(private val lottoService: LottoService) {
+
+    private val logger = LoggerFactory.getLogger(javaClass)
+
+    @GetMapping("get-lucky-number")
+    fun getLotto(@RequestParam count: Int): ResponseEntity<*> {
+        logger.info("getLuckyNumber{}", count)
+        return ResponseEntity.ok(
+            GetLuckyNumRspDTO.builder()
+                .luckyNumber(lottoService.createLottoNumber(count))
+                .build()
+        )
+    }
+
+    @GetMapping("win-percentage")
+    @Throws(ParseException::class)
+    fun getWinPercentage(@RequestParam drwNo: Int): ResponseEntity<*> {
+        return ResponseEntity.ok(lottoService.getWinPercentage(drwNo))
     }
 }
